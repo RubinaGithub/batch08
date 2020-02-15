8f0da29956ff518b4b465730eeb2a0c1369487d8
Rubina Parvin Noor <rubina.parvin941@gmail.com>
2020-02-15 16:07:37 +0600

    class 12

--- routes/web.php
+++ routes/web.php
@@ -10,32 +10,41 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-use Illuminate\Http\Request;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+use Illuminate\http\Request;
+/*Route::get('/', function () {
+ 
+    return view('welcome');
+});*/
 
 Route::get('/','HomeController@index');
+
 Route::resource('/product','ProductController');
-Route::view('/about','about');
-
-// Route::view('/registration','registration');
-// Route::resource('product', 'ProductController');
-// Route::post('/submit-product', function (Request $request) {
-//     $data = array('product_name'=>$request->product_name, 'product_price'=>$request->product_price);
-//     return view('/show_product_details',['data'=>$data]);
-// });
-Route::view('/contact_us','contact_us');
-Route::resource('product', 'ProductController');
-Route::post('product/submitRating','ProductCOntroller@submitRating');
-Route::post('/submit-contact', function (Request $request) {
-    $data = array('name'=>$request->user_name, 'email'=>$request->email, 'message'=>$request->message);
-    return view('/show_details',['data'=>$data]);
-});
-
-Route::post('/add-to-cart','CartController@addToCart');
+//cart
 Route::get('/mycart','CartController@mycart');
+Route::post('/add-to-cart','CartController@addToCart');
 Route::post('/update-cart','CartController@updateCart');
 Route::any('/cartItemDelete/{temp_order_row_id}','CartController@cartItemDelete');
 Route::any('/cartItemDeleteAll','CartController@cartItemDeleteAll');
+
+
+//end cart
+
+
+Route::get ('/checkoutPage','CartController@checkoutItems');
+Route::post ('/confirm-order','CartController@confirmOrder');
+Route::post ('/checkout','CartController@checkout');
+Route::post ('/checkoutwithregistration','CartController@checkoutwithregistration');
+
+Route::get('/user-registration','Auth\CommonController@showRegistrationForm')->name('user.registration');
+Route::post('/user-registration','Auth\CommonController@register')->name('user.registration.submit');
+
+Route::view('/about', 'about');
+
+Route::view('/contact_us', 'contact');
+//Route::view('/thankyou,'thankyou');
+Route::post('/submit-contact', function (Request $request){
+//	dd($request);
+	$data = array('name'=>$request->username,'email' => $request->email, 'message' => $request->message);
+
+	return view('/show_details',['data'=>$data]);
+});
